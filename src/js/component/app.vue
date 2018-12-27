c3f94d56267e01811ddaaef17ed573e8f6a05f7f
zolotykh <alexandergold@yandex.ru>
2018-12-27 11:21:30 +0300

    router transitions

--- src/js/component/app.vue
+++ src/js/component/app.vue
@@ -2,7 +2,10 @@
   <div class="app">
     <app-header></app-header>
 
-    <router-view></router-view>
+    <transition name="fade"
+                mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
