163a3d9c1d3bd52ff14a63f4c60ee7ddb62c4b1f
zolotykh <alexandergold@yandex.ru>
2018-12-27 13:46:51 +0300

    Code docs

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,25 @@
+/**
+ *
+ * @type {string} css-selector
+ */
 const APPLICATION_SELECTOR = '#app';
+
+/**
+ * database name
+ * @type {string}
+ */
 const STORAGE_KEY = 'kanban-db';
+
+/**
+ * If true - demo board will be displayed
+ * @type {boolean}
+ */
 const USE_MOCK_DATA_FALLBACK = true;
+
+/**
+ * Demo access key
+ * @type {string}
+ */
 const ACCESS_TOKEN = '37ddc985c8a02ded34a6a541f790642b4d9d02f9c9e3fd05e3ec762eecc914cb';
 
 export { APPLICATION_SELECTOR, STORAGE_KEY, USE_MOCK_DATA_FALLBACK, ACCESS_TOKEN };
